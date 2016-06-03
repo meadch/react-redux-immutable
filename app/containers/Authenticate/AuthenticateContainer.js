f1eac32d6fc0602710c232272cb7e455bb7992b9
meadch <meadch@gmail.com>
2016-06-03 13:55:17 -0700

    redux-connection

--- app/containers/Authenticate/AuthenticateContainer.js
+++ app/containers/Authenticate/AuthenticateContainer.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import { Authenticate } from 'components'
 import auth from 'helpers/auth'
+import { connect } from 'react-redux'
 
 const AuthenticateContainer = React.createClass({
+  propTypes: {
+    isFetching: PropTypes.bool.isRequired,
+    error: PropTypes.string.isRequired,
+  },
   handleAuth () {
     auth().then( (user) => {
       console.log("Authed User: ", user)
     })
   },
   render () {
+    console.log("PROPS: ", this.props)
     return (
       <Authenticate
         isFetching={false}
@@ -18,4 +24,11 @@ const AuthenticateContainer = React.createClass({
   },
 })
 
-export default AuthenticateContainer
+function mapStateToProps(state){
+  return {
+    isFetching: state.isFetching,
+    error: state.error,
+  }
+}
+
+export default connect(mapStateToProps)(AuthenticateContainer)
--- app/index.js
+++ app/index.js
@@ -1,4 +1,17 @@
+import React from 'react'
 import ReactDOM from 'react-dom'
 import routes from './config/routes'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import users from 'redux/modules/users'
 
-ReactDOM.render(routes, document.getElementById('app'))
+const store = createStore(users)
+
+const app = (
+  // Provider provides a way to give each component a piece of the store
+  <Provider store={store}>
+    {routes}
+  </Provider>
+)
+
+ReactDOM.render(app, document.getElementById('app'))
